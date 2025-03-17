2d0045925ebcd83ec91a94474d7750ffa93d0a88
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-03-17 16:31:14 +0500

    CF: +CF transform

--- ContinuedFractions/ContinuedFraction.cs
+++ ContinuedFractions/ContinuedFraction.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Numerics;
 using System.Text;
+using System.Diagnostics.CodeAnalysis;
+
 
 namespace ContinuedFractions;
 
@@ -9,18 +11,31 @@ namespace ContinuedFractions;
 /// </summary>
 public readonly partial struct ContinuedFraction {
 
-  private readonly IEnumerable<int> _kConvergent;
+  private readonly IEnumerable<int> _cf;
 
-  private ContinuedFraction(IEnumerable<int> cf) { _kConvergent = cf; }
+  public ContinuedFraction(IEnumerable<int> cf) { _cf = cf; }
 
-  public IEnumerable<(BigInteger numerator, BigInteger denominator)> FromCF() => FromCF(_kConvergent);
+  public IEnumerable<(BigInteger numerator, BigInteger denominator)> FromCF() => FromCF(_cf);
 
-  public IEnumerable<Matrix22> kConvergentMatrix() {
-    Matrix22 res = Matrix22.Id();
+  private static Matrix22 LFT_step(Matrix22 m, int a) => m * Matrix22.Homographic(a);
 
-    foreach (int kC in _kConvergent) {
-      res *= Matrix22.Homographic(kC);
-      yield return res;
+  public ContinuedFraction CF_transform(Matrix22 init) => new ContinuedFraction(CF_transform_main(init));
+
+  public IEnumerable<int> CF_transform_main(Matrix22 init) { // todo: IEnumerable<(int b, Matrix22 hfunc)>
+    Matrix22 m = init;
+    foreach (int a in _cf) {
+      m = LFT_step(m, a);
+
+      foreach ((int q, Matrix22 r) gcd in GCD(m)) {
+        m = gcd.r;
+        yield return gcd.q;
+      }
+    }
+
+    // Если исходная непрерывная дробь была конечной, то надо выполнить ещё раз алгоритм Евклида.
+    if (m[2] != 0) {
+      foreach (int i in GCD(m[0], m[2]))
+        yield return i;
     }
   }
 
@@ -34,7 +49,7 @@ public readonly partial struct ContinuedFraction {
     StringBuilder res = new StringBuilder("[");
 
     // Берем только до 40 элементов для отображения: чтобы по абсолютной точности соответствовать типу double
-    var elementsToString = _kConvergent.Take(41).ToList();
+    var elementsToString = _cf.Take(41).ToList();
 
     if (elementsToString.Count == 0) {
       return res.Append(']').ToString();
@@ -105,6 +120,72 @@ public readonly partial struct ContinuedFraction {
 
       numerator   = denominator;
       denominator = remainder;
+    }
+  }
+
+  private static bool GCD_step(Matrix22 m, [NotNullWhen(true)] out int? q, [NotNullWhen(true)] out Matrix22? r) {
+    if (m[2] == 0 && m[3] == 0) {
+      throw new ArgumentException($"GCD_step: That series has already ended. Found: the second row of matrix is zero.");
+    }
+
+    q = null;
+    r = null;
+
+    if (m[2] == 0 || m[3] == 0) { // Функция неограниченна, недостаточно информации для определения частного
+      return false;
+    }
+
+    if (m[2] != 0 && m[3] < 0) { // Есть ноль в области определения
+      return false;
+    }
+
+    // Функция ограничена
+    double n0 = (double)m[0] / m[2];
+    double n1 = (double)m[1] / m[3];
+
+    double v0 = Math.Min(n0, n1);
+    double v1 = Math.Max(n0, n1);
+
+    int d0 = (int)Math.Floor(v0);
+    int d1 = (int)Math.Floor(v1);
+
+    // если d1 == d0, то частное определяется однозначно,
+    // если d1 отличается больше чем на 1 от d0, то ничего определённого сказать нельзя
+    // если d1 == d0 + 1 и d1 == v1, то эта граница достигается только либо в 0, либо в oo; поэтому ответ d0.
+    if (d1 == d0 || (d1 == d0 + 1 && Math.Abs(d1 - v1) < 1e-14)) {
+      q = d0;
+      r = new Matrix22(m[2], m[3], m[0] - d0 * m[2], m[1] - d0 * m[3]);
+
+      return true;
+    }
+
+    return false;
+  }
+
+  private static IEnumerable<int> GCD(int a, int b) {
+    while (b != 0) {
+      int q = Math.DivRem(a, b, out int r);
+
+      yield return q;
+
+      a = b;
+      b = r;
+    }
+  }
+
+  public static IEnumerable<(int q, Matrix22 r)> GCD(Matrix22 m) {
+    while (true) {
+      if (m[0] == 0 && m[1] == 0) { // числитель равен нулю
+        break;
+      }
+
+      if (!GCD_step(m, out int? q, out Matrix22? r)) {
+        break;
+      }
+
+      yield return ((int)q, (Matrix22)r);
+
+      m = (Matrix22)r;
     }
   }
 #endregion
