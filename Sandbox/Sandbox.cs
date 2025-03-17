b457a3aabb28273bea2b12108a1149486e2cb279
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-03-17 21:33:43 +0500

    Add Sandbox project

---
+++ Sandbox/Sandbox.cs
@@ -0,0 +1,17 @@
+﻿using ContinuedFractions;
+namespace Sandbox;
+
+
+public class Sandbox {
+
+  public static void Main(string[] args) {
+    Console.WriteLine($"Hi");
+  }
+  // public static void Main() {
+  //   // var x = ContinuedFraction.E();
+  //   var x = new ContinuedFraction(new int[] { 1, 2, 3 });
+  //
+  //   Console.WriteLine($"{x.FromCF().Last().numerator} / {x.FromCF().Last().denominator}");
+  // }
+
+}
