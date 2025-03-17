d496f64f1eead385c58ef2d8418cddf2b7987ba3
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-03-17 21:34:51 +0500

    CompareTo

---
+++ ContinuedFractions/Comparation.cs
@@ -0,0 +1,61 @@
+﻿namespace ContinuedFractions;
+
+
+public readonly partial struct ContinuedFraction : IComparable<ContinuedFraction>, IEquatable<ContinuedFraction> {
+
+  private const int compCut = 100;
+
+
+  public int CompareTo(ContinuedFraction other) {
+    using var cf_this = _cf.GetEnumerator();
+    using var cf_other = other._cf.GetEnumerator();
+
+    int index = 0;
+    while (index < compCut) {
+      bool next1 = cf_this.MoveNext();
+      bool next2 = cf_other.MoveNext();
+
+      if (!next1 && !next2) {
+        return 0;
+      }
+
+      if (!next1) { // смотри дерево Штерна-Броко
+        return cf_other.Current;
+      }
+
+      if (!next2) {
+        return -cf_this.Current;
+      }
+
+      int val1 = cf_this.Current;
+      int val2 = cf_other.Current;
+
+      if (index % 2 != 0) { // для нечетных индексов меняем знак
+        val1 = -val1;
+        val2 = -val2;
+      }
+
+      if (val1 < val2) {
+        return -1;
+      }
+      if (val1 > val2) {
+        return 1;
+      }
+
+      index++;
+    }
+
+    return 0;
+  }
+
+  public bool Equals(ContinuedFraction other) => this.CompareTo(other) == 0;
+
+  public override bool Equals(object? obj) {
+    return obj is ContinuedFraction other && Equals(other);
+  }
+
+  public override int GetHashCode() { // можно договориться, что кэш берём с первых 40 элементов
+    throw new NotImplementedException();
+  }
+
+}
---
+++ Tests/ComparationTests.cs
@@ -0,0 +1,36 @@
+﻿using ContinuedFractions;
+using NUnit.Framework;
+
+namespace Tests;
+
+[TestFixture]
+public class ComparationTests {
+
+
+  [Test]
+  public void EQTest() {
+    var r001 = ContinuedFraction.FromRational(1,1);
+    var r010 = ContinuedFraction.FromRational(1,2);
+    var r011 = ContinuedFraction.FromRational(2,1);
+    var r100 = ContinuedFraction.FromRational(1,3);
+    var r110 = ContinuedFraction.FromRational(2,3);
+    var r101 = ContinuedFraction.FromRational(3,2);
+    var r111 = ContinuedFraction.FromRational(3,1);
+
+
+
+    Assert.That(r001.Equals(r001));
+    Assert.That(r001.Equals(r010) is false);
+
+    Assert.That(ContinuedFraction.E().Equals(ContinuedFraction.E()));
+    Assert.That(ContinuedFraction.E().Equals(ContinuedFraction.Sqrt2()) is false);
+
+    Assert.That(r011.CompareTo(r101) > 0);
+    Assert.That(r100.CompareTo(r110) < 0);
+    Assert.That(r111.CompareTo(r011) > 0);
+    Assert.That(r011.CompareTo(r111) < 0);
+
+    // todo: Больше сравнений!
+  }
+
+}
