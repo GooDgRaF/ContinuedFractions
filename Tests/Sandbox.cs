29096efbb178e6af9656d85b1e85f0371fc929b1
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-03-17 16:30:15 +0500

    Update Sandbox.cs

--- Tests/Sandbox.cs
+++ Tests/Sandbox.cs
@@ -5,13 +5,10 @@ namespace Tests;
 public class Sandbox {
 
   public static void Main() {
-    // Console.WriteLine($"{ContinuedFraction.FromRational(66,13)}");
-    var x = ContinuedFraction.E();
-    // var x = new ContinuedFraction(new List<int>(){2,1,1,5,1});
+    // var x = ContinuedFraction.E();
+    var x = new ContinuedFraction(new List<int>(){2,1,1,5,1});
     var y = x.CF_transform(new Matrix22(2,0,0,1));
-    foreach (int i in y.Take(10)) {
-      Console.Write($"{i} ");
-    }
+    Console.WriteLine($"{y}");
   }
 
 }
