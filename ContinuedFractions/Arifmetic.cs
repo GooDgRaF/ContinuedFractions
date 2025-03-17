debe917d6641551233143ee7971247f64e7f9e62
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-03-17 21:35:06 +0500

    Add

---
+++ ContinuedFractions/Arifmetic.cs
@@ -0,0 +1,9 @@
+﻿namespace ContinuedFractions;
+
+
+public readonly partial struct ContinuedFraction{
+
+  public static ContinuedFraction operator +(ContinuedFraction cf, Frac frac)
+    => cf.CF_transform(new Matrix22(frac.q, frac.p, 0, frac.q));
+
+}
---
+++ Tests/OperatorTests.cs
@@ -0,0 +1,16 @@
+﻿using ContinuedFractions;
+using NUnit.Framework;
+
+namespace Tests;
+
+[TestFixture]
+public class OperatorTests {
+
+  public static ContinuedFraction cfFin1 = new ContinuedFraction(new int[] { 1, 2, 3 }); // 10 / 7
+
+  [Test]
+  public void AddFracTest() {
+    var r01 = cfFin1 + new Frac(0, 1);
+  }
+
+}
