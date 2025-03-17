5427df526ee0c7c9201e798547627782332c0819
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-03-17 16:25:36 +0500

    Update Sandbox.cs

--- Tests/Sandbox.cs
+++ Tests/Sandbox.cs
@@ -4,25 +4,14 @@ namespace Tests;
 
 public class Sandbox {
 
-  public static void gcd(int a, int b, int c, int d) {
-    // a*x + b = q * (c*x + d) + r
-
-    while (c != 0) {
-      int q  = a / c;
-      int r1 = a - q * c;
-      int r2 = b - q * d;
-
-      Console.WriteLine($"{a}*x + {b}");
-
-      a = c;
-      b = d;
-      c = r1;
-      d = r2;
+  public static void Main() {
+    // Console.WriteLine($"{ContinuedFraction.FromRational(66,13)}");
+    var x = ContinuedFraction.E();
+    // var x = new ContinuedFraction(new List<int>(){2,1,1,5,1});
+    var y = x.CF_transform(new Matrix22(2,0,0,1));
+    foreach (int i in y.Take(10)) {
+      Console.Write($"{i} ");
     }
-
-    Console.WriteLine($"{a}*x + {b}");
   }
-
-  public static void Main() { gcd(6, 8, 4, 6); }
 
 }
