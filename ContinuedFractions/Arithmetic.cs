fbead84e0350b4a824c2ce7e97dcaa6bc7fb00d9
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-03-18 15:03:25 +0500

    rename

---
+++ ContinuedFractions/Arithmetic.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+
+namespace ContinuedFractions;
+
+
+public readonly partial struct ContinuedFraction :
+  // IUnaryNegationOperators<ContinuedFraction, ContinuedFraction> ,
+ IUnaryPlusOperators<ContinuedFraction,ContinuedFraction>
+{
+
+  public static ContinuedFraction operator +(ContinuedFraction value) => value;
+
+  // public static ContinuedFraction operator -(ContinuedFraction value) { // хз что это такое пока что
+  //   throw new NotImplementedException();
+  // }
+
+  public static ContinuedFraction operator +(ContinuedFraction cf, Frac frac)
+    => cf.CF_transform(new Matrix22(frac.q, frac.p, 0, frac.q));
+
+  public static ContinuedFraction operator +(Frac frac, ContinuedFraction cf) => cf + frac;
+
+}
