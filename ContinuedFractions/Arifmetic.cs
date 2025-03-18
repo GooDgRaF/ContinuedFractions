20fbfe78887b2912dbad8bdf6886687543b13979
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-03-18 12:41:41 +0500

    Update Arifmetic.cs

--- ContinuedFractions/Arifmetic.cs
+++ ContinuedFractions/Arifmetic.cs
@@ -1,7 +1,13 @@
-﻿namespace ContinuedFractions;
+﻿using System.Numerics;
+
+namespace ContinuedFractions;
 
 
-public readonly partial struct ContinuedFraction{
+public readonly partial struct ContinuedFraction : IUnaryNegationOperators<ContinuedFraction, ContinuedFraction> {
+
+  public static ContinuedFraction operator -(ContinuedFraction value) {
+    throw new NotImplementedException();
+  }
 
   public static ContinuedFraction operator +(ContinuedFraction cf, Frac frac)
     => cf.CF_transform(new Matrix22(frac.q, frac.p, 0, frac.q));
