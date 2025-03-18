f0ff524cbbe2293e90f3dece5161968372997d73
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-03-18 12:41:08 +0500

    Update Sandbox.cs

--- Sandbox/Sandbox.cs
+++ Sandbox/Sandbox.cs
@@ -5,13 +5,10 @@ namespace Sandbox;
 public class Sandbox {
 
   public static void Main(string[] args) {
-    Console.WriteLine($"Hi");
+
+    ContinuedFraction x = ContinuedFraction.E().CF_transform(new Matrix22(2,0,0,1));
+    Console.WriteLine($"{x}");
   }
-  // public static void Main() {
-  //   // var x = ContinuedFraction.E();
-  //   var x = new ContinuedFraction(new int[] { 1, 2, 3 });
-  //
-  //   Console.WriteLine($"{x.FromCF().Last().numerator} / {x.FromCF().Last().denominator}");
-  // }
+
 
 }
