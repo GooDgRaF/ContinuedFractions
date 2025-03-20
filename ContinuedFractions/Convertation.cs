b3dff30c5de353ac43013bbeab1d48f42c75740f
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-03-20 15:16:37 +0500

    Create Convertation.cs

---
+++ ContinuedFractions/Convertation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Diagnostics;
+using System.Numerics;
+using System.Text;
+using System.Diagnostics.CodeAnalysis;
+
+
+namespace ContinuedFractions;
+
+public readonly partial struct CFraction
+{
+  public IEnumerable<(BigInteger numerator, BigInteger denominator)> FromCF() => FromCF(_cfCashed);
+
+
+      private static IEnumerable<(BigInteger numerator, BigInteger denominator)> FromCF(IEnumerable<int> cf) {
+        BigInteger p1 = 1;
+        BigInteger p0 = 0;
+        BigInteger q1 = 0;
+        BigInteger q0 = 1;
+
+        foreach (int coeff in cf) {
+          BigInteger p = coeff * p1 + p0;
+          BigInteger q = coeff * q1 + q0;
+
+          yield return (p, q);
+
+          p0 = p1;
+          p1 = p;
+          q0 = q1;
+          q1 = q;
+        }
+      }
+}
