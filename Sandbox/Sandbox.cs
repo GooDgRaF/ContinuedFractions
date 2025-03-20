8beb27d74286b02131a7783bbb851d9d3288d282
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-03-20 15:16:15 +0500

    Update Sandbox.cs

--- Sandbox/Sandbox.cs
+++ Sandbox/Sandbox.cs
@@ -6,8 +6,6 @@ public class Sandbox {
 
   public static void Main(string[] args) {
 
-    ContinuedFraction x = ContinuedFraction.E().CF_transform(new Matrix22(2,0,0,1));
-    Console.WriteLine($"{x}");
   }
 
 
