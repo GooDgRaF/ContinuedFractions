d85ac49f64a31622cee0710e5ac87a2f0f7a6e0e
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-03-21 15:23:00 +0500

    Update IndexerTests.cs

--- Tests/IndexerTests.cs
+++ Tests/IndexerTests.cs
@@ -1,12 +1,45 @@
 ﻿namespace Tests;
 
 [TestFixture]
-public class CFractionIndexerTests {
+public class IndexerTests {
 
   [Test]
-  public void Indexer_ValidIndexWithinCache_ReturnsCachedNullableValue() {
+  public void Indexer_OneElements() {
+    List<int> coeffs   = new List<int> { 1 };
+    CFraction fraction = CFraction.FromGenerator(coeffs);
+
+    int? actualValue = fraction[0];
+
+    Assert.That(actualValue.HasValue, Is.True, "Indexer should return a nullable value with a value");
+    Assert.That(actualValue.Value, Is.EqualTo(1), "Indexer should return the first coefficient for index 0");
+  }
+
+  [Test]
+  public void Indexer_TwoElementsOnes() {
+    List<int> coeffs   = new List<int> { 1, 1 };
+    CFraction fraction = CFraction.FromGenerator(coeffs);
+
+    int? actualValue = fraction[0];
+
+    Assert.That(actualValue.HasValue, Is.True, "Indexer should return a nullable value with a value");
+    Assert.That(actualValue.Value, Is.EqualTo(2), "Indexer should return the digit 2");
+  }
+
+  [Test]
+  public void Indexer_TwoElements() {
+    List<int> coeffs   = new List<int> { 1, 2 };
+    CFraction fraction = CFraction.FromGenerator(coeffs);
+
+    int? actualValue = fraction[0];
+
+    Assert.That(actualValue.HasValue, Is.True, "Indexer should return a nullable value with a value");
+    Assert.That(actualValue.Value, Is.EqualTo(1), "Indexer should return the first coefficient for index 0");
+  }
+
+  [Test]
+  public void Indexer_ValidIndexWithinCache() {
     List<int> coeffs   = new List<int> { 1, 2, 3, 4, 5 };
-    CFraction fraction = CFraction.FromCoeffs(coeffs);
+    CFraction fraction = CFraction.FromGenerator(coeffs);
     fraction.Take(3);
 
     int? actualValue = fraction[1];
@@ -15,10 +48,11 @@ public class CFractionIndexerTests {
     Assert.That(actualValue.Value, Is.EqualTo(2), "Indexer should return cached value for index within cache");
   }
 
+
   [Test]
-  public void Indexer_ValidIndexOutsideCache_CachesAndReturnsNullableValue() {
+  public void Indexer_ValidIndexOutsideCache() {
     List<int> coeffs   = new List<int> { 1, 2, 3, 4, 5 };
-    CFraction fraction = CFraction.FromCoeffs(coeffs);
+    CFraction fraction = CFraction.FromGenerator(coeffs);
     fraction.Take(2);
 
     int? actualValue = fraction[3];
@@ -29,68 +63,13 @@ public class CFractionIndexerTests {
   }
 
   [Test]
-  public void Indexer_IndexZero_ReturnsFirstNullableCoefficient() {
-    List<int> coeffs   = new List<int> { 10, 20, 30 };
-    CFraction fraction = CFraction.FromCoeffs(coeffs);
-
-    int? actualValue = fraction[0];
-
-    Assert.That(actualValue.HasValue, Is.True, "Indexer should return a nullable value with a value");
-    Assert.That(actualValue.Value, Is.EqualTo(10), "Indexer should return the first coefficient for index 0");
-  }
-
-  [Test]
-  public void Indexer_LargeValidIndex_ReturnsCorrectNullableValue() {
-    List<int> coeffs   = Enumerable.Range(1, 100).ToList();
-    CFraction fraction = CFraction.FromCoeffs(coeffs);
-
-    int? actualValue = fraction[99];
-
-    Assert.That(actualValue.HasValue, Is.True, "Indexer should return a nullable value with a value");
-    Assert.That(actualValue.Value, Is.EqualTo(100), "Indexer should handle large valid indices");
-  }
-
-  [Test]
   public void Indexer_IndexAtEnd_ReturnsNull() {
     List<int> coeffs   = new List<int> { 1, 2, 3 };
-    CFraction fraction = CFraction.FromCoeffs(coeffs);
+    CFraction fraction = CFraction.FromGenerator(coeffs);
 
     int? actualValue = fraction[3]; // Index at the end
 
     Assert.That(actualValue, Is.Null, "Indexer should return null for index at the end of CFraction");
-  }
-
-  [Test]
-  public void Indexer_IndexOutOfRange_ThrowsIndexOutOfRangeException() {
-    List<int> coeffs   = new List<int> { 1, 2, 3 };
-    CFraction fraction = CFraction.FromCoeffs(coeffs);
-
-    Assert.Throws<IndexOutOfRangeException>
-      (
-       ()
-         => {
-         var temp = fraction[4];
-       }
-     , // Index out of range
-       "Indexer should throw IndexOutOfRangeException for index out of range"
-      );
-  }
-
-  [Test]
-  public void Indexer_IndexOutOfRangeAfterPartialCache_ThrowsIndexOutOfRangeException() {
-    List<int> coeffs   = new List<int> { 1, 2, 3 };
-    CFraction fraction = CFraction.FromCoeffs(coeffs);
-    fraction.Take(2);
-
-    Assert.Throws<IndexOutOfRangeException>
-      (
-       ()
-         => {
-         var temp = fraction[4];
-       }
-     , // Index out of range
-       "Indexer should throw IndexOutOfRangeException for index out of range even after partial cache"
-      );
   }
 
   [Test]
