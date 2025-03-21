132ebacda69acd2b1b9fd33e89066b413481805f
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-03-21 16:59:40 +0500

    +

--- ContinuedFractions/Constants.cs
+++ ContinuedFractions/Constants.cs
@@ -2,9 +2,10 @@
 
 public partial struct CFraction {
 
-  public static CFraction E     = new CFraction(EGenerator());
-  public static CFraction Sqrt2 = new CFraction(Sqrt2Generator());
-  public static CFraction Infinity = new CFraction(new List<int>());
+  public static CFraction E        = new CFraction(EGenerator());
+  public static CFraction Sqrt2    = new CFraction(Sqrt2Generator());
+  public static CFraction Infinity = new CFraction(new int[] { });
+  public static CFraction Zero     = new CFraction(new int[] { 0 });
 
 
   private static IEnumerable<int> EGenerator() {
@@ -23,6 +24,7 @@ public partial struct CFraction {
 
   private static IEnumerable<int> Sqrt2Generator() {
     yield return 1;
+
     while (true) {
       yield return 2;
     }
