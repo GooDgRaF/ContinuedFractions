fbd544c57a096bac04aaa723c718abd376727e71
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-03-28 16:30:15 +0500

    Update Sandbox.cs

--- Sandbox/Sandbox.cs
+++ Sandbox/Sandbox.cs
@@ -1,12 +1,16 @@
-﻿using ContinuedFractions;
-namespace Sandbox;
+﻿using System.Threading.Channels;
+using ContinuedFractions;
 
+namespace Sandbox;
 
 public class Sandbox {
 
-  public static void Main(string[] args) {
-    Console.WriteLine($"{(double)CFraction.Infinity}");
-  }
+  public static string CFPrint(CFraction cf) => $"{cf}\t\t== {(double)cf}";
 
+  public static void Main(string[] args) {
+    CFraction cf = CFraction.FromGenerator(new int[] { -1, 1, 1 }.Concat(Enumerable.Repeat(2, 100)));
+    Console.WriteLine($"{(double)cf}");
+    Console.WriteLine($"{-double.Sqrt(2)+1}");
+  }
 
 }
