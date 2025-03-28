fe6f7183cf981a249afd14833b1173b9e87409f7
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-03-28 14:01:11 +0500

    + negative value tests

--- Tests/ComparationTests.cs
+++ Tests/ComparationTests.cs
@@ -3,18 +3,29 @@
 [TestFixture]
 public class ComparationTests {
 
-  private static readonly CFraction r001 = CFraction.FromRational(1, 1); // 1   = [1]
-  private static readonly CFraction r010 = CFraction.FromRational(1, 2); // 1/2 = [0;2]
-  private static readonly CFraction r011 = CFraction.FromRational(2, 1); // 2   = [2]
-  private static readonly CFraction r100 = CFraction.FromRational(1, 3); // 1/3 = [0;3]
-  private static readonly CFraction r110 = CFraction.FromRational(2, 3); // 2/3 = [0;1,2]
-  private static readonly CFraction r101 = CFraction.FromRational(3, 2); // 3/2 = [1;2]
-  private static readonly CFraction r111 = CFraction.FromRational(3, 1); // 3   = [3]
+  private static readonly CFraction r001 = CFraction.FromRational(1, 1);     // 1   = [1]
+  private static readonly CFraction r010 = CFraction.FromRational(1, 2);     // 1/2 = [0;2]
+  private static readonly CFraction r011 = CFraction.FromRational(2, 1);     // 2   = [2]
+  private static readonly CFraction r100 = CFraction.FromRational(1, 3);     // 1/3 = [0;3]
+  private static readonly CFraction r110 = CFraction.FromRational(2, 3);     // 2/3 = [0;1,2]
+  private static readonly CFraction r101 = CFraction.FromRational(3, 2);     // 3/2 = [1;2]
+  private static readonly CFraction r111 = CFraction.FromRational(3, 1);     // 3   = [3]
+
+  private static readonly CFraction rn010 = CFraction.FromRational(-1, 2);   // -1/2 = [-1;2]
+  private static readonly CFraction rn100 = CFraction.FromRational(-1, 3);   // -1/3 = [-1; 1, 2]
+  private static readonly CFraction rn011 = CFraction.FromRational(-2, 3);   // -2/3 = [-1;3]
+  private static readonly CFraction rn101 = CFraction.FromRational(-3, 2);   // -3/2 = [-2;2]
+  private static readonly CFraction rn111 = CFraction.FromRational(-3, 1);   // -3   = [-3]
+  private static readonly CFraction rn001 = CFraction.FromRational(-1, 1);   // -1   = [-1]
+  private static readonly CFraction rn002 = CFraction.FromRational(-2, 1);   // -2   = [-2]
 
   [Test]
   public void Equals() {
     Assert.That(r001.Equals(r001));          // 1 == 1
     Assert.That(r001.Equals(r010) is false); // 1 != 1/2
+    Assert.That(rn010.Equals(rn010));        // -1/2 == -1/2
+    Assert.That(rn010.Equals(r010) is false); // -1/2 != 1/2
+
 
     var r1 = CFraction.FromRational(5, 7); // 5/7 = [0;1,2,2]
     var r2 = CFraction.FromRational(5, 7);
@@ -22,6 +33,7 @@ public class ComparationTests {
 
     Assert.That(CFraction.E.Equals(CFraction.E));              // e == e
     Assert.That(CFraction.E.Equals(CFraction.Sqrt2) is false); // e != sqrt(2)
+    Assert.That(CFraction.Zero.Equals(CFraction.Zero));        // 0 == 0
   }
 
   [Test]
@@ -30,6 +42,12 @@ public class ComparationTests {
     Assert.That(r100.CompareTo(r110) < 0); // 1/3 < 2/3
     Assert.That(r111.CompareTo(r011) > 0); // 3 > 2
     Assert.That(r011.CompareTo(r111) < 0); // 2 < 3
+
+    Assert.That(rn010.CompareTo(rn100) < 0); // -1/2 < -1/3  (|-1/2| > |-1/3|)
+    Assert.That(rn100.CompareTo(rn010) > 0); // -1/3 > -1/2
+    Assert.That(rn111.CompareTo(rn002) < 0); // -3 < -2
+    Assert.That(rn002.CompareTo(rn111) > 0); // -2 > -3
+
   }
 
   [Test]
@@ -74,40 +92,83 @@ public class ComparationTests {
 
   [Test]
   public void Compare_DiffAtOddIndex() {
-    // 1/2 = [0; 2], 1/3 = [0; 3]
-    var r1 = r010;
-    var r2 = r100;
+    // 1/2 = [0; 2], 1/3 = [0; 3]  <- was [0;2] vs [0;3], now [-1;2] vs [-1; 1, 2]
+    var r1 = r010;   // 1/2 = [0; 2]
+    var r2 = r100;   // 1/3 = [0; 3]
     Assert.That(r1.CompareTo(r2) > 0); // 1/2 > 1/3
   }
 
-  // [Test]
-  // public void Compare_Negative_r1() {
-  //   var r1 = -CFraction.FromRational(1, 2);
-  //   var r2 = CFraction.FromRational(1, 2);
-  //   Assert.That(r1.CompareTo(r2) < 0);
-  // }
-  //
-  // [Test]
-  // public void Compare_Negative_r2() {
-  //   var r1 = CFraction.FromRational(1, 2);
-  //   var r2 = -CFraction.FromRational(1, 2);
-  //   Assert.That(r1.CompareTo(r2) > 0);
-  // }
-  //
-  // [Test]
-  // public void Compare_Negative_Both() {
-  //   var r1 = -CFraction.FromRational(1, 2);
-  //   var r2 = -CFraction.FromRational(1, 3);
-  //   Assert.That(r1.CompareTo(r2) < 0); // -1/2 < -1/3
-  // }
-  //
-  // [Test]
-  // public void Compare_Zero() {
-  //   var r1 = CFraction.Zero;
-  //   var r2 = CFraction.FromRational(1, 2);
-  //   Assert.That(r1.CompareTo(r2) < 0);
-  //   Assert.That(r2.CompareTo(r1) > 0);
-  //   Assert.That(r1.CompareTo(CFraction.Zero) == 0);
-  // }
+  [Test]
+  public void Compare_Negative_r1() {
+    var r1 = -CFraction.FromRational(1, 2); // -1/2 = [-1; 2]
+    var r2 = CFraction.FromRational(1, 2);  // 1/2  = [0; 2]
+    Assert.That(r1.CompareTo(r2) < 0);      // -1/2 < 1/2
+  }
 
+  [Test]
+  public void Compare_Negative_r2() {
+    var r1 = CFraction.FromRational(1, 2);     // 1/2  = [0; 2]
+    var r2 = -CFraction.FromRational(1, 2);    // -1/2 = [-1; 2]
+    Assert.That(r1.CompareTo(r2) > 0);         // 1/2 > -1/2
+  }
+
+  [Test]
+  public void Compare_Negative_Both() {
+    var r1 = -CFraction.FromRational(1, 2);    // -1/2 = [-1; 2]
+    var r2 = -CFraction.FromRational(1, 3);    // -1/3 = [-1; 1, 2]
+    Assert.That(r1.CompareTo(r2) < 0);         // -1/2 < -1/3 (|-1/2| > |-1/3|)
+  }
+
+  [Test]
+  public void Compare_Zero_vs_Positive() {
+    var r1 = CFraction.Zero;                   // 0    = [0]
+    var r2 = CFraction.FromRational(1, 2);     // 1/2  = [0; 2]
+    Assert.That(r1.CompareTo(r2) < 0);         // 0 < 1/2
+    Assert.That(r2.CompareTo(r1) > 0);         // 1/2 > 0
+  }
+
+  [Test]
+  public void Compare_Zero_vs_Negative() {
+    var r1 = CFraction.Zero;                   // 0     = [0]
+    var r2 = -CFraction.FromRational(1, 2);    // -1/2  = [-1; 2]
+    Assert.That(r1.CompareTo(r2) > 0);         // 0 > -1/2
+    Assert.That(r2.CompareTo(r1) < 0);         // -1/2 < 0
+  }
+
+  [Test]
+  public void Compare_Zero_Equals_Zero() {
+    Assert.That(CFraction.Zero.CompareTo(CFraction.Zero) == 0); // 0 == 0
+  }
+
+  [Test]
+  public void Compare_NegativeInteger_vs_NegativeFraction() {
+    var r1 = rn001;                             // -1   = [-1]
+    var r2 = rn101;                             // -3/2 = [-2; 2]
+    Assert.That(r1.CompareTo(r2) > 0);         // -1 > -3/2  (-1 > -1.5)
+    Assert.That(r2.CompareTo(r1) < 0);         // -3/2 < -1
+  }
+
+  [Test]
+  public void Compare_NegativeFraction_vs_NegativeInteger() {
+    var r1 = rn101;                             // -3/2 = [-2; 2]
+    var r2 = rn001;                             // -1   = [-1]
+    Assert.That(r1.CompareTo(r2) < 0);         // -3/2 < -1
+    Assert.That(r2.CompareTo(r1) > 0);         // -1 > -3/2
+  }
+
+  [Test]
+  public void Compare_NegativeInteger_vs_SmallerNegativeInteger() {
+    var r1 = rn001;                             // -1   = [-1]
+    var r2 = rn002;                             // -2   = [-2]
+    Assert.That(r1.CompareTo(r2) > 0);         // -1 > -2
+    Assert.That(r2.CompareTo(r1) < 0);         // -2 < -1
+  }
+
+  [Test]
+  public void Compare_PositiveFraction_vs_NegativeFraction() {
+    var r1 = r010;                              // 1/2  = [0; 2]
+    var r2 = rn010;                             // -1/2 = [-1; 2]
+    Assert.That(r1.CompareTo(r2) > 0);         // 1/2 > -1/2
+    Assert.That(r2.CompareTo(r1) < 0);         // -1/2 < 1/2
+  }
 }
--- Tests/FabricTests.cs
+++ Tests/FabricTests.cs
@@ -5,14 +5,15 @@ namespace Tests;
 [TestFixture]
 public class FactoryTests {
 
+#region FromRational
   [Test]
   public void FromRational_One() {
     BigInteger numerator      = 1;
     BigInteger denominator    = 1;
     List<int>  expectedCoeffs = new List<int> { 1 };
 
-    CFraction fraction     = CFraction.FromRational(numerator, denominator);
-    var       actualCoeffs = fraction.Take(expectedCoeffs.Count);
+    CFraction cf           = CFraction.FromRational(numerator, denominator);
+    var       actualCoeffs = cf.Take(expectedCoeffs.Count);
 
     Assert.That(actualCoeffs, Is.EqualTo(expectedCoeffs), "Coefficients for 1/1 should be [1]");
   }
@@ -23,8 +24,8 @@ public class FactoryTests {
     BigInteger denominator    = 7;
     List<int>  expectedCoeffs = new List<int> { 0 };
 
-    CFraction fraction     = CFraction.FromRational(numerator, denominator);
-    var       actualCoeffs = fraction.Take(expectedCoeffs.Count);
+    CFraction cf           = CFraction.FromRational(numerator, denominator);
+    var       actualCoeffs = cf.Take(expectedCoeffs.Count);
 
     Assert.That(actualCoeffs, Is.EqualTo(expectedCoeffs), "Coefficients for 0/7 should be [0]");
   }
@@ -35,11 +36,86 @@ public class FactoryTests {
     BigInteger denominator    = 7;
     List<int>  expectedCoeffs = new List<int> { 3, 7 };
 
-    CFraction fraction     = CFraction.FromRational(numerator, denominator);
-    var       actualCoeffs = fraction.Take(expectedCoeffs.Count);
+    CFraction cf           = CFraction.FromRational(numerator, denominator);
+    var       actualCoeffs = cf.Take(expectedCoeffs.Count);
 
     Assert.That(actualCoeffs, Is.EqualTo(expectedCoeffs), "Coefficients for 22/7 should be [3, 7]");
   }
+
+  [Test]
+  public void FromRational_NegativeFraction_CorrectCoeffs() {
+    BigInteger numerator      = -22;
+    BigInteger denominator    = 7;
+    List<int>  expectedCoeffs = new List<int> { -4, 1, 6 };
+
+    CFraction cf           = CFraction.FromRational(numerator, denominator);
+    var       actualCoeffs = cf.Take(expectedCoeffs.Count);
+
+    Assert.That(actualCoeffs, Is.EqualTo(expectedCoeffs), "Coefficients for -22/7 should be [-4, 1, 6]");
+  }
+
+  [Test]
+  public void FromRational_NegativeDenominator_CorrectCoeffs() {
+    BigInteger numerator      = 22;
+    BigInteger denominator    = -7;
+    List<int>  expectedCoeffs = new List<int> { -4, 1, 6 };
+
+    CFraction cf           = CFraction.FromRational(numerator, denominator);
+    var       actualCoeffs = cf.Take(expectedCoeffs.Count);
+
+    Assert.That(actualCoeffs, Is.EqualTo(expectedCoeffs), "Coefficients for 22/-7 should be [-4, 1, 6]");
+  }
+
+  [Test]
+  public void FromRational_BothNegative_CorrectCoeffs() {
+    BigInteger numerator      = -22;
+    BigInteger denominator    = -7;
+    List<int>  expectedCoeffs = new List<int> { 3, 7 };
+
+    CFraction cf           = CFraction.FromRational(numerator, denominator);
+    var       actualCoeffs = cf.Take(expectedCoeffs.Count);
+
+    Assert.That(actualCoeffs, Is.EqualTo(expectedCoeffs), "Coefficients for -22/-7 should be [3, 7]");
+  }
+
+  [Test]
+  public void FromRational_FractionLessThanOne_CorrectCoeffs() {
+    BigInteger numerator      = 7;
+    BigInteger denominator    = 22;
+    List<int>  expectedCoeffs = new List<int> { 0, 3, 7 };
+
+    CFraction cf           = CFraction.FromRational(numerator, denominator);
+    var       actualCoeffs = cf.Take(expectedCoeffs.Count);
+
+    Assert.That(actualCoeffs, Is.EqualTo(expectedCoeffs), "Coefficients for 7/22 should be [0, 3, 7]");
+  }
+
+  [Test]
+  public void FromRational_ReducibleFraction_CorrectCoeffs() {
+    BigInteger numerator      = 44;
+    BigInteger denominator    = 14;
+    List<int>  expectedCoeffs = new List<int> { 3, 7 };
+
+    CFraction cf           = CFraction.FromRational(numerator, denominator);
+    var       actualCoeffs = cf.Take(expectedCoeffs.Count);
+
+    Assert.That(actualCoeffs, Is.EqualTo(expectedCoeffs), "Coefficients for 44/14 should be [3, 7]");
+  }
+
+  [Test]
+  public void FromRational_Infinity() {
+    Assert.That(CFraction.Infinity, Is.EqualTo(CFraction.FromRational(1, 0)), "The fraction 1/0 should be equal to infinity.");
+  }
+
+  [Test]
+  public void FromRational_ZeroByZero_ThrowsArgumentException() {
+    BigInteger numerator   = 0;
+    BigInteger denominator = 0;
+
+    Assert.Throws<ArgumentException>
+      (() => CFraction.FromRational(numerator, denominator).Take(1), "Expected ArgumentException for 0/0 fraction");
+  }
+
 
   [Test]
   public void FromRational_Overflow_ThrowsException() {
@@ -52,14 +128,16 @@ public class FactoryTests {
      , "Expected OverflowException if coefficient is out of int.MaxValue range"
       );
   }
+#endregion
 
+#region FromCoeffs
   [Test]
   public void FromCoeffs_ValidCoeffs() {
     List<int> inputCoeffs    = new List<int> { 1, 2, 3, 4 };
     List<int> expectedCoeffs = inputCoeffs;
 
-    CFraction fraction     = CFraction.FromCoeffs(inputCoeffs);
-    var       actualCoeffs = fraction.Take(expectedCoeffs.Count);
+    CFraction cf           = CFraction.FromCoeffs(inputCoeffs);
+    var       actualCoeffs = cf.Take(expectedCoeffs.Count);
 
     Assert.That(actualCoeffs, Is.EqualTo(expectedCoeffs), "For valid coefficients, Take() should return the same");
   }
@@ -77,8 +155,8 @@ public class FactoryTests {
     List<int> validCoeffsWithZeroFirst = new List<int> { 0, 2, 3, 4 };
     List<int> expectedCoeffs           = validCoeffsWithZeroFirst;
 
-    CFraction fraction     = CFraction.FromCoeffs(validCoeffsWithZeroFirst);
-    var       actualCoeffs = fraction.Take(expectedCoeffs.Count);
+    CFraction cf           = CFraction.FromCoeffs(validCoeffsWithZeroFirst);
+    var       actualCoeffs = cf.Take(expectedCoeffs.Count);
 
     Assert.That(actualCoeffs, Is.EqualTo(expectedCoeffs), "Zero as the first coefficient should be valid");
   }
@@ -88,20 +166,56 @@ public class FactoryTests {
     List<int> emptyCoeffs    = new List<int>();
     List<int> expectedCoeffs = emptyCoeffs;
 
-    CFraction fraction     = CFraction.FromCoeffs(emptyCoeffs);
-    var       actualCoeffs = fraction.Take(expectedCoeffs.Count);
+    CFraction cf           = CFraction.FromCoeffs(emptyCoeffs);
+    var       actualCoeffs = cf.Take(expectedCoeffs.Count);
 
     Assert.That(actualCoeffs, Is.EqualTo(expectedCoeffs), "Empty coefficients list should be valid (infinity cf)");
+    Assert.That(cf, Is.EqualTo(CFraction.Infinity), "Empty coefficients list should be valid (infinity cf)");
   }
 
+  [Test]
+  public void FromCoeffs_SingleCoeffList() {
+    List<int> singleCoeffList = new List<int> { 5 };
+    List<int> expectedCoeffs  = singleCoeffList;
+
+    CFraction cf           = CFraction.FromCoeffs(singleCoeffList);
+    var       actualCoeffs = cf.Take(expectedCoeffs.Count);
+
+    Assert.That(actualCoeffs, Is.EqualTo(expectedCoeffs), "Single coefficient list should be valid");
+  }
+
+  [Test]
+  public void FromCoeffs_ListEndsWithOne() {
+    List<int> inputCoeffs    = new List<int> { 5, 1 };
+    List<int> expectedCoeffs = new List<int> { 6 };
+
+    CFraction cf           = CFraction.FromCoeffs(inputCoeffs);
+    var       actualCoeffs = cf.Take(expectedCoeffs.Count);
+
+    Assert.That(actualCoeffs, Is.EqualTo(expectedCoeffs), "List ending with 1 should be canonicalized");
+  }
+
+  [Test]
+  public void FromCoeffs_NegativeFirstCoeff() {
+    List<int> inputCoeffs    = new List<int> { -1, 2, 3 };
+    List<int> expectedCoeffs = inputCoeffs;
+
+    CFraction cf           = CFraction.FromCoeffs(inputCoeffs);
+    var       actualCoeffs = cf.Take(expectedCoeffs.Count);
+
+    Assert.That(actualCoeffs, Is.EqualTo(expectedCoeffs), "Negative first coefficient should be valid");
+  }
+#endregion
+
+#region FromGenerator
   [Test]
   public void FromGenerator_List() {
     List<int>        coeffs         = new List<int> { 1, 2, 3 };
     IEnumerable<int> generator      = coeffs;
     List<int>        expectedCoeffs = coeffs;
 
-    CFraction fraction     = CFraction.FromGenerator(generator);
-    var       actualCoeffs = fraction.Take(expectedCoeffs.Count);
+    CFraction cf           = CFraction.FromGenerator(generator);
+    var       actualCoeffs = cf.Take(expectedCoeffs.Count);
 
     Assert.That(actualCoeffs, Is.EqualTo(expectedCoeffs), "FromGenerator should work with List<int>");
   }
@@ -112,8 +226,8 @@ public class FactoryTests {
     IEnumerable<int> generator      = coeffs;
     List<int>        expectedCoeffs = new List<int> { 1, 2, 4 };
 
-    CFraction fraction     = CFraction.FromGenerator(generator);
-    var       actualCoeffs = fraction.Take(expectedCoeffs.Count);
+    CFraction cf           = CFraction.FromGenerator(generator);
+    var       actualCoeffs = cf.Take(expectedCoeffs.Count);
 
     Assert.That(actualCoeffs, Is.EqualTo(expectedCoeffs), "The [1;2,3,1] should be the same as [1;2,4]");
   }
@@ -124,8 +238,8 @@ public class FactoryTests {
     IEnumerable<int> generator      = coeffs;
     List<int>        expectedCoeffs = coeffs.ToList();
 
-    CFraction fraction     = CFraction.FromGenerator(generator);
-    var       actualCoeffs = fraction.Take(expectedCoeffs.Count);
+    CFraction cf           = CFraction.FromGenerator(generator);
+    var       actualCoeffs = cf.Take(expectedCoeffs.Count);
 
     Assert.That(actualCoeffs, Is.EqualTo(expectedCoeffs), "FromGenerator should work with int[]");
   }
@@ -135,29 +249,29 @@ public class FactoryTests {
     IEnumerable<int> generator      = GetTestGenerator();
     List<int>        expectedCoeffs = new List<int> { 1, 2, 3 };
 
-    CFraction fraction     = CFraction.FromGenerator(generator);
-    var       actualCoeffs = fraction.Take(expectedCoeffs.Count);
+    CFraction cf           = CFraction.FromGenerator(generator);
+    var       actualCoeffs = cf.Take(expectedCoeffs.Count);
 
     Assert.That(actualCoeffs, Is.EqualTo(expectedCoeffs), "FromGenerator should work with IEnumerable<int> generator");
   }
+
+  [Test]
+  public void FromGenerator_EmptyGenerator() {
+    IEnumerable<int> generator      = Enumerable.Empty<int>();
+    List<int>        expectedCoeffs = new List<int>();
+
+    CFraction cf           = CFraction.FromGenerator(generator);
+    var       actualCoeffs = cf.Take(expectedCoeffs.Count);
+
+    Assert.That(actualCoeffs, Is.EqualTo(expectedCoeffs), "FromGenerator should work with empty generator");
+  }
+#endregion
+
 
   private static IEnumerable<int> GetTestGenerator() {
     yield return 1;
     yield return 2;
     yield return 3;
   }
-
-  // [Test]
-  // public void FromRational_NegativeFraction_CorrectCoeffs()
-  // {
-  //     BigInteger numerator = -22;
-  //     BigInteger denominator = 7;
-  //     List<int> expectedCoeffs = new List<int> { -4, 1, 6 };
-  //
-  //     CFraction fraction = CFraction.FromRational(numerator, denominator);
-  //     var actualCoeffs = fraction.Take(expectedCoeffs.Count);
-  //
-  //     Assert.That(actualCoeffs, Is.EqualTo(expectedCoeffs), "Coefficients for -22/7 should be [-4, 1, 6]");
-  // }
 
 }
