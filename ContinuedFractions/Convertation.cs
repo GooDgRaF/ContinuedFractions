dcac076d5e2accff7e1fb49679ba0973c737481a
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2025-04-01 19:24:16 +0500

    Update Convertation.cs

--- ContinuedFractions/Convertation.cs
+++ ContinuedFractions/Convertation.cs
@@ -12,16 +12,16 @@ public partial class CFraction {
   public IEnumerable<(BigInteger numerator, BigInteger denominator)> ToRational() => ToRational(_cfCashed);
 
   private static IEnumerable<(BigInteger numerator, BigInteger denominator)> ToRational(IEnumerable<int> cf) {
-    decimal p1 = 1;
-    decimal p0 = 0;
-    decimal q1 = 0;
-    decimal q0 = 1;
+    BigInteger p1 = 1;
+    BigInteger p0 = 0;
+    BigInteger q1 = 0;
+    BigInteger q0 = 1;
 
     foreach (int coeff in cf) {
-      decimal p = coeff * p1 + p0;
-      decimal q = coeff * q1 + q0;
+      BigInteger p = coeff * p1 + p0;
+      BigInteger q = coeff * q1 + q0;
 
-      yield return ((BigInteger)p, (BigInteger)q);
+      yield return (p, q);
 
       p0 = p1;
       p1 = p;
